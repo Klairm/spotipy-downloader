948c14e2cd53ccbe9a7f71b875823405b376b382
Klairm <ivangardis@hotmail.com>
2021-06-10 19:56:28 +0200

    Skip song if was already downloaded

--- spoti_downloader.py
+++ spoti_downloader.py
@@ -1,6 +1,7 @@
 import spotipy
 import sys
 import json
+import os
 from spotipy.oauth2 import SpotifyClientCredentials
 from youtubesearchpython import VideosSearch
 from pytube import YouTube
@@ -14,11 +15,11 @@ except FileNotFoundError:
 
     print('''File credentials.json not found, please create one with the following syntax:
 {
-	"client_id":"your_client_id_here",
-	"client_secret":"your_client_secret_id_here"
+    "client_id":"your_client_id_here",
+    "client_secret":"your_client_secret_id_here"
 }
 
-		''')
+        ''')
 
 if len(sys.argv) <= 1:
     # Path is optional, by default it will be the current one.
@@ -45,7 +46,6 @@ total = sp.playlist_items(playlist_url, fields='total').get('total')
 def completed(artist, song_name):
     print(f"Downloaded {artist} - {song_name}")
 
-
 while offset < total:
     if offset == total:
         break
@@ -54,5 +54,10 @@ while offset < total:
     song_name = sp.playlist_items(playlist_url, offset=offset, fields='items.track.name').get(
         'items')[0].get('track').get('name')
     offset += 1
-    yt_link = VideosSearch(f"{artist} {song_name}", limit=1).result().get('result')[0].get('link')
-    YouTube(yt_link, on_complete_callback=completed(artist, song_name)).streams.get_audio_only().download(output_path=download_path, filename=f"{artist} - {song_name}")
+    if os.path.exists(f"{download_path}/{artist} - {song_name}.mp4"):
+        print(f"Skip existing song... {artist} - {song_name}")
+    else:
+        yt_link = VideosSearch(f"{artist} {song_name}", limit=1).result().get('result')[0].get('link')
+        song = YouTube(yt_link,on_complete_callback=completed(artist, song_name)).streams.get_audio_only()
+        song.download(output_path=download_path, filename=f"{artist} - {song_name}")
+
